a7504bfcc4ef07d02ac189f73cb596b3cfa89a4d
Christopher Reed <84865108+mr-chris-reed@users.noreply.github.com>
2022-10-10 14:11:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# need this import statement so Python can
+# generate random numbers
+import random
+
+# give an overview to user the purpose of program
+# hint: use some print statements
+"""
+your code goes here
+"""
+
+# ask the user for their name,
+# the lower limit,
+# and the upper limit
+# hint 1: use the input function
+# hint 2: you will need to convert the user
+# input for lower and upper limits into integers
+"""
+your code goes here
+"""
+
+# write a function named generateRandomNum
+# parameters will be lower, upper
+# returns a random number in range of
+# lower - upper, inclusive
+# hint: will need to use random.randint(lower, upper)
+"""
+your code goes here
+"""
+
+# write a function called printResult that will
+# print the result to the user in a statement
+# that includes their name, the lower and upper
+# limits, and the result for the random number
+# hint: you will need to call the generateRandomNum
+# function and you will need to convert the values
+# of lower, upper, and randNum back to strings.
+"""
+your code goes here
+"""
+
+# make a call to your printResult() function
+"""
+your code goes here
+"""
